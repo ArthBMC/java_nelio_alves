a0984ad469a555969b5c7673e211c4e87b3dc51a
Arthur Barbosa <arthurbarbosaabab261@gmail.com>
2025-12-27 18:53:49 -0300

    usando trycatch pela primeira vez

---
+++ src/section14/tryCatch/application/program.java
@@ -0,0 +1,29 @@
+package section14.tryCatch.application;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in).useLocale(Locale.US);
+
+        try {
+            String[] vector = in.nextLine().split(" ");
+            int position = in.nextInt();
+            System.out.println(vector[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position!");
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Input error");
+        }
+
+
+        System.out.println("End of program");
+
+
+        in.close();
+    }
+}
