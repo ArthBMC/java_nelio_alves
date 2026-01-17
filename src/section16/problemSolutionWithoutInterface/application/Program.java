6077b97e790be83adfd8ce710bbc739cea6e195e
Arthur Barbosa <arthurbarbosaabab261@gmail.com>
2026-01-16 23:19:14 -0300

    Starting a new section, and making a program without Interface yet

---
+++ src/section16/problemSolutionWithoutInterface/application/Program.java
@@ -0,0 +1,48 @@
+package section16.problemSolutionWithoutInterface.application;
+
+import section16.problemSolutionWithoutInterface.entities.CarRental;
+import section16.problemSolutionWithoutInterface.entities.Invoice;
+import section16.problemSolutionWithoutInterface.entities.Vehicle;
+import section16.problemSolutionWithoutInterface.model.services.BrazilTaxService;
+import section16.problemSolutionWithoutInterface.model.services.RentalService;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in).useLocale(Locale.US);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        System.out.println("Enter with the rent data: ");
+        System.out.print("Vehicle model: ");
+        String vehicleModel = in.nextLine();
+        System.out.print("Start rental date (dd/MM/yyyy hh:mm): ");
+        LocalDateTime start = LocalDateTime.parse((in.nextLine()), fmt);
+        System.out.print("Finish rental date (dd/MM/yyyy hh:mm): ");
+        LocalDateTime finish = LocalDateTime.parse((in.nextLine()), fmt);
+
+        CarRental carRental = new CarRental(start, finish, new Vehicle(vehicleModel));
+
+        System.out.print("Enter with the price per hour: ");
+        double pricePerHour = in.nextDouble();
+        System.out.print("Enter with the price per day: ");
+        double pricePerDay = in.nextDouble();
+
+        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+
+        rentalService.processInvoice(carRental);
+
+        System.out.println("INVOICE:");
+        System.out.println("Basic Payment: " + carRental.getInvoice().getBasicPayment());
+        System.out.println("Tax: " + carRental.getInvoice().getTax());
+        System.out.println("Total Payment: " + carRental.getInvoice().getTotalPayment());
+
+
+
+        in.close();
+    }
+}
